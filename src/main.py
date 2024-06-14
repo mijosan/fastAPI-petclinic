bee875f36cd001642da00b843d8dfdf4ce33eca7
Taesan Choi <mijosan@naver.com>
2024-06-14 10:19:44 +0900

    fix main.py에 패키지 인식하도록 sys 등록

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import sys
 import os
 
-# src 디렉토리를 sys.path에 추가
-sys.path.append(os.path.join(os.path.dirname(__file__)))
+# 프로젝트 루트 경로를 sys.path에 추가
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 import uvicorn
-from .app_config import create_app
+from src.app_config import create_app
 
 app = create_app()
 
@@ -14,4 +14,4 @@ def read_root():
     return {"FastAPI PetClinic Sample Application"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
