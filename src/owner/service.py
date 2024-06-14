db6750a171757b7ee567361a798daad62948be4e
Taesan Choi <mijosan@naver.com>
2024-06-14 15:05:16 +0900

    feat db 트랜잭션 코드 추가

--- src/owner/service.py
+++ src/owner/service.py
@@ -1,5 +1,6 @@
 
 from typing import List
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 from sqlalchemy.future import select
 from .exceptions import OwnerNotFoundException
@@ -11,30 +12,42 @@ class OwnerService:
         self.db = db
 
     def get_owners(self, owner_request: OwnerRequest) -> List[OwnerResponse]:
-        stmt = select(Owner)
+        try: 
+            with self.db.begin(): 
+                stmt = select(Owner)
+                
+                # 조건부 필터링
+                if owner_request.name:
+                    stmt = stmt.where(Owner.name.like(f"%{owner_request.name}%"))
+                
+                # 페이지네이션 적용
+                stmt = stmt.offset(owner_request.skip).limit(owner_request.limit)
+                
+                owners = self.db.execute(stmt).scalars().all()
+                
+                # SQLAlchemy 객체를 Pydantic 객체로 변환
+                owner_list = [SchemaOwner.from_orm(owner) for owner in owners]
         
-        # 조건부 필터링
-        if owner_request.name:
-            stmt = stmt.where(Owner.name.like(f"%{owner_request.name}%"))
-        
-        # 페이지네이션 적용
-        stmt = stmt.offset(owner_request.skip).limit(owner_request.limit)
-        
-        owners = self.db.execute(stmt).scalars().all()
-        
-        # SQLAlchemy 객체를 Pydantic 객체로 변환
-        owner_list = [SchemaOwner.from_orm(owner) for owner in owners]
-    
-        return OwnerResponse(owners=owner_list)
-
+                return OwnerResponse(owners=owner_list)
+        except SQLAlchemyError:
+            self.db.rollback()
+            
+            raise Exception
+            
     def get_owner(self, owner_id: int) -> OwnerResponse:
-        stmt = select(Owner).where(Owner.id == owner_id)
-        
-        owner = self.db.execute(stmt).scalar_one_or_none()
-        
-        if owner is None:
-            raise OwnerNotFoundException(owner_id)
+        try: 
+            with self.db.begin(): 
+                stmt = select(Owner).where(Owner.id == owner_id)
+                
+                owner = self.db.execute(stmt).scalar_one_or_none()
+                
+                if owner is None:
+                    raise OwnerNotFoundException(owner_id)
 
-        owner_list = [owner]
+                owner_list = [owner]
 
-        return OwnerResponse(owners=owner_list)
+                return OwnerResponse(owners=owner_list)
+        except SQLAlchemyError:
+            self.db.rollback()
+            
+            raise Exception
