6e266a28f6e5edbc0e6bf706bf8b039ad455fe83
Taesan Choi <mijosan@naver.com>
2024-06-21 17:30:20 +0900

    refactor 리팩토링

--- src/user/service.py
+++ src/user/service.py
@@ -1,6 +1,4 @@
-from fastapi import HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
-from passlib.context import CryptContext
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.future import select
 from sqlalchemy.orm import Session, joinedload
@@ -24,32 +22,37 @@ class UserService:
         try:
             with self.db.begin():
                 # id 중복 체크
-                stmt = select(UserModel).where(UserModel.id == user_request.id)
-                existing_user = self.db.execute(stmt).scalar_one_or_none()
+                existing_user = self.db.query(UserModel).filter(UserModel.id == user_request.id).one_or_none()
                 if existing_user:
                     raise UserIdAlreadyExistsException(user_request.id)
 
                 # email 중복 체크
-                stmt = select(UserModel).where(UserModel.email == user_request.email)
-                existing_email = self.db.execute(stmt).scalar_one_or_none()
+                existing_email = self.db.query(UserModel).filter(UserModel.email == user_request.email).one_or_none()
                 if existing_email:
                     raise UserEmailAlreadyExistsException(user_request.email)
                 
-                default_role = self.db.execute(select(RoleModel).where(RoleModel.id == 2)).scalar_one_or_none()
+                default_role = self.db.query(RoleModel).filter(RoleModel.id == 2).one_or_none()
                 
                 user_model = UserModel(
-                    id = user_request.id,
-                    email = user_request.email,
-                    password = get_password_hash(user_request.password),
+                    id=user_request.id,
+                    email=user_request.email,
+                    password=get_password_hash(user_request.password),
                 )
                 
                 self.db.add(user_model)
     
                 # 기본 역할 추가
-                user_model.roles.append(default_role)
+                if default_role:
+                    user_model.roles.append(default_role)
                 
                 user_list = []
-                user_list.append(UserSchema.from_orm(user_model))
+                
+                # flush는 세션의 변경 사항을 데이터베이스에 반영하지만 트랜잭션을 커밋하지 않습니다. 
+                # 이 작업은 데이터베이스의 ID와 같은 자동 증가 값을 즉시 사용할 수 있도록 보장합니다. 
+                # flush는 데이터베이스에 데이터를 실제로 기록하므로, 이후에 새로 생성된 객체의 ID를 사용할 수 있습니다.
+                # 단 여기서는 ID를 직접 넣기 때문에 필요없음
+                self.db.flush()
+                user_list.append(UserSchema.model_validate(user_model))
                 
                 self.db.commit()
 
